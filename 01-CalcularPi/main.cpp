56416d1f2d6971f15ce07b5d7938c7b074c1c721
Tomás Hokama <thokama@frba.utn.edu.ar>
2023-06-01 22:18:01 -0300

    Add files via upload

---
+++ 01-CalcularPi/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double sumatoria = 0;
+    int iteraciones = 1000000;
+
+    for (int n = 0; n < iteraciones; n++) {
+        double termino = (n % 2 == 0) ? 1.0 / (2*n + 1) : -1.0 / (2*n + 1);
+        sumatoria += termino;
+    }
+
+    double pi = 4 * sumatoria;
+    cout.precision(7);
+    cout << "El valor de pi es: " << pi << endl;
+    return 0;
+}
